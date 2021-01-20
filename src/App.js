f57ffa2ba36bea797e6d6a8d7a5b12253ffaf487
Caty Chapman <caty@cherkesky.com>
2021-01-20 14:03:39 -0600

    feature branch created and changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, lovely :)
+          Hello, darlin' :)
         </p>
         <a
           className="App-link"
